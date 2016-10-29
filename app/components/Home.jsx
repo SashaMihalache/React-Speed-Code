0b9093216b8a6439fb89ffac1475dc2c67b44c14
Sasha Mihalache <sasha.mihalache@gmail.com>
2016-10-29 14:49:21 +0300

    Define component internals

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,23 +2,25 @@ import React, { PropTypes, Component } from 'react';
 import World from './World';
 import Card from './Card';
 import Youtube from './Youtube';
+import IconText from './IconText';
+import Github from './Github';
 
 export default class Home extends Component {
   render() {
-    return(
+    return (
       <section className="landing">
         <nav className="nav">
           <a className="nav__brand" href="/">
             ReactSpeed
           </a>
           <a className="nav__link">
-              Book
+            Book
           </a>
           <a className="nav__link">
-              Code
+            Code
           </a>
           <a className="nav__link">
-              Demos
+            Demos
           </a>
           <a className="nav__link">
             Website
@@ -40,6 +42,51 @@ export default class Home extends Component {
           </Card>
         </section>
 
+        // Github
+        <section className="stripe">
+          <Card className="col--quarter text--center back--default">
+            <Github repo="angular/angular" />
+          </Card>
+          <Card className="col--quarter text--center back--default">
+            <Github repo="facebook/react" />
+          </Card>
+          <Card className="col--quarter text--center back--default">
+            <Github repo="vuejs/vue" />
+          </Card>
+        </section>
+
+        <section className="stripe back--default">
+          <Card className="col--one-third text--center back--white">
+            <IconText className="primary" icon="globe" size="5x"
+              text="Nine Component Creation Strategies" />
+          </Card>
+          <Card className="col--one-fourth back--white">
+            <h3>
+              <IconText slim className="danger" icon="building"
+                text="Modular Architecture" />
+            </h3>
+            <h3>
+              <IconText slim className="default" icon="cloud"
+                text="Leverages Cloud" />
+            </h3>
+            <h3>
+              <IconText slim className="secondary" icon="cog"
+                text="30 Custom Components" />
+            </h3>
+            <h3>
+              <IconText slim className="warning" icon="bullseye"
+                text="Goal Oriented Design" />
+            </h3>
+          </Card>
+          <Card plain className="col--one-third text--center">
+            <h1>Infographic Components</h1>
+            <p className="subtext">
+              Custom React components easily reusable to
+                create variety of Infographic elements.
+            </p>
+          </Card>
+        </section>
+
         <section className="stripe">
           <Card plain className="col--one-third text--center back--white">
             <Youtube videoid="9cMhWPID38c" />
@@ -51,6 +98,9 @@ export default class Home extends Component {
             </p>
           </Card>
         </section>
+
+
+
 
         <section className="stripe back--default">
           <Card plain className="col--half text--center">
@@ -66,8 +116,8 @@ export default class Home extends Component {
         </section>
         <section className="stripe--slim back--gray">
           <Card plain className="col--full text--center white">
-          <p>
-            Copyright (c) 2016, Manav Sehgal.
+            <p>
+              Copyright (c) 2016, Sasha Mihalache.
             All rights reserved.
           </p>
           </Card>
