b6df8b6a3f9a0c442068a126e1186552a839c6e1
Sasha Mihalache <sasha.mihalache@gmail.com>
2016-11-15 20:59:32 +0200

    refactor on icons svgs

--- app/components/AboutCustom.jsx
+++ app/components/AboutCustom.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import Card from './Card';
 import World from './World';
+import TodoApp from './TodoApp';
 
 const AboutCustom = () => (
-    <section className="stripe pattern-agsquare">
-        <Card
-            className="col--one-third text--center back--white">
-            <World />
-        </Card>
-        <Card plain className="col--half text--center">
-            <h1>Custom React Component</h1>
-            <p className="subtext">
-                This custom component demonstrates props, state,
+    <section>
+        <section className="stripe">
+            <Card
+                className="col--one-third text--center back--white">
+                <TodoApp />
+            </Card>
+            <Card plain className="col--half text--center">
+                <h1>ES5 To ES6 Conversion </h1>
+                <p className="default">
+                    This custom component is a conversion from ES5 to ES6,
+                    reusing ReactSpeed UI Library.
+                </p>
+            </Card>
+        </section>
+        <section className="stripe pattern-agsquare">
+            <Card
+                className="col--one-third text--center back--white">
+                <World />
+            </Card>
+            <Card plain className="col--half text--center">
+                <h1>Custom React Component</h1>
+                <p className="subtext">
+                    This custom component demonstrates props, state,
         and ES6 classes.
       </p>
-        </Card>
+            </Card>
+        </section>
     </section>
 );
 
--- app/components/IconText.jsx
+++ app/components/IconText.jsx
@@ -1,73 +1,75 @@
 import React, { PropTypes } from 'react';
 
 const IconText = (props) => {
-  const {
-    icon, text, size, rotate,
-    flip, inverse, slim, ribbon,
-    active, ...rest } = props;
+	const {
+		icon, text, size, rotate,
+		flip, inverse, slim, ribbon,
+		active, ...rest } = props;
 
+	let variation = '';
 
-  let variation = '';
+	variation += size ? ` fa-${size}` : '';
+	variation += rotate ? ` fa-rotate-${rotate}` : '';
+	variation += flip ? ` fa-flip-${flip}` : '';
+	variation += inverse ? ' fa-inverse' : '';
 
-  variation += size ? ` fa-${size}` : '';
-  variation += rotate ? ` fa-rotate-${rotate}` : '';
-  variation += flip ? ` fa-flip-${flip}` : '';
-  variation += inverse ? ' fa-inverse' : '';
+	const iconClass = `fa fa-${icon}${variation}`;
 
-  const iconClass = `fa fa-${icon}${variation}`;
+	let renderIcon = null;
 
-  let renderIcon = null;
+	if (slim) {
+		renderIcon = (
+			<div {...rest}>
+				<i className={iconClass} /> {text}
+			</div>);
+	}
 
-  if (slim) {
-    renderIcon =
-      <div {...rest} >
-        <i className={iconClass}></i> {text}
-      </div>;
-  }
+	if (ribbon) {
+		renderIcon = (
+			<div {...rest}
+				className={`${props.className} ribbon__menu text--center`}>
+				<i className={iconClass} />
+				<p className={active
+					? 'ribbon__text--active'
+					: 'ribbon__text'}>
+					{text}
+				</p>
+			</div>
+		);
+	}
 
-  if (ribbon) {
-    renderIcon =
-      <div {...rest} className={`${props.className} ribbon__menu text--center`}>
-        <i className={iconClass}></i>
-        <p className={active
-          ? 'ribbon__text--active'
-          : 'ribbon__text'}>
-          {text}
-        </p>
-      </div>;
-  }
+	if (!slim && !ribbon) {
+		renderIcon = (
+			<div {...rest}>
+				<i className={iconClass} />
+				<h1>{text}</h1>
+			</div>
+		);
+	}
 
-  if (!slim && !ribbon) {
-    renderIcon =
-      <div {...rest}>
-        <i className={iconClass}></i>
-        <h1>{text}</h1>
-      </div>;
-  }
-
-  return (renderIcon);
+	return (renderIcon);
 };
 
 IconText.propTypes = {
-  icon: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  className: PropTypes.string,
-  size: PropTypes.oneOf(['lg', '2x', '3x', '4x', '5x', '']),
-  rotate: PropTypes.number,
-  flip: PropTypes.oneOf(['horizontal', 'vertical', '']),
-  inverse: PropTypes.bool,
-  slim: PropTypes.bool,  // draw slim single-line InfoText
-  ribbon: PropTypes.bool
+	icon: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
+	className: PropTypes.string,
+	size: PropTypes.oneOf(['lg', '2x', '3x', '4x', '5x', '']),
+	rotate: PropTypes.number,
+	flip: PropTypes.oneOf(['horizontal', 'vertical', '']),
+	inverse: PropTypes.bool,
+	slim: PropTypes.bool,  // draw slim single-line InfoText
+	ribbon: PropTypes.bool
 };
 
 IconText.defaultProps = {
-  className: '',
-  size: '',
-  rotate: null,
-  flip: '',
-  inverse: false,
-  slim: false,
-  ribbon: false
+	className: '',
+	size: '',
+	rotate: null,
+	flip: '',
+	inverse: false,
+	slim: false,
+	ribbon: false
 };
 
 
