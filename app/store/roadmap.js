0595412b5cfd9f0d076519578822f2230d3c5143
Sasha Mihalache <sasha.mihalache@gmail.com>
2016-11-28 21:53:45 +0200

    added redux dev tools

--- app/store/roadmap.js
+++ app/store/roadmap.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
+import devToolsEnhancer from 'remote-redux-devtools';
 import roadmapApp from '../reducers/roadmapApp';
 
-const store = createStore(roadmapApp);
+const store = createStore(roadmapApp, devToolsEnhancer());
 export default store;
